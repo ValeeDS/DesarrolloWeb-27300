8187b1f19769b8df3ae988142fad235e014f510c
Valeria Del Savio <valeria.delsavio@hotmail.com>
2021-12-26 22:55:28 -0300

    Clase 16

---
+++ Clase 16/After/email.php
@@ -0,0 +1,54 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'c2460249.ferozo.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'test@c2460249.ferozo.com';                     //SMTP username
+    $mail->Password   = 'Comision@27300';                         //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //Recipients
+    $mail->setFrom('test@c2460249.ferozo.com', 'Comision 27300'); // Hacer coincidir con el username. (preferentemente)
+    $mail->addAddress('lc77278@gmail.com', 'Luis');
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Consulta de: '.$_POST['inp_nombre'];
+    $mail->Body    = 
+        'Nombre: '.$_POST['inp_nombre'].
+        '<br>Apellido: '.$_POST['inp_apellido'].
+        '<br>Email: '.$_POST['inp_email'].
+        '<br>Direccion: '.$_POST['inp_dir'].
+        '<br>Mensaje: '.$_POST['inp_mensaje'];
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    header("location: gracias.html");
+} catch (Exception $e) {
+    //echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    header("location: error.html");
+}
+
+?>
